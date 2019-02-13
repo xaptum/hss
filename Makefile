bc986a3da206c7314fe559c58a87c605f1ddb426
Jeroen Z <Jeroen.Zeelmaekers@dab-embedded.com>
2019-02-13 10:49:17 +0100

    Added skeleton for proxy part on both sides

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 obj-m += xarpcd.o 
-xarpcd-objs := xarpcd_main.o xarpcd_socket.o xarpcd_proxy.o xarpcd_usb.o
+xarpcd-objs := xarpcd_main.o xarpcd_socket.o xarpcd_proxy.o xarpcd_usb.o 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
--- xarpcd_proxy.c
+++ xarpcd_proxy.c
@@ -1,3 +1,7 @@
+/**
+ *
+ */
+
 #include "../../common/psock_proxy_msg.h"
 
 int xarpcd_proxy_init( void )
@@ -6,4 +10,33 @@ int xarpcd_proxy_init( void )
 	return 0;
 }
 
+/****************************************************************************
+ * Api towards the usb handling code
+ ***************************************************************************/
+
+int xarpcd_proxy_pop_in_msg( void )
+{
+	return 0;
+}
+
+int xarpcd_proxy_push_out_msg( void )
+{
+	return 0;
+}
+
+
+
+/***************************************************************************
+ * Api towards the socket handling code
+ ***************************************************************************/
+
+int xarpcd_proxy_get_next_request( void )
+{
+	return 0;
+}
+
+int xarpcd_proxy_put_next_reply( void )
+{
+	return 0;
+}
 
