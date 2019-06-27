30c959eeb9d6c75bf1b27f71787649f2b408a083
Daniel Berliner <daniel.berliner@xaptum.com>
2019-06-27 11:24:49 -0500

    Fix vendor ID

--- xarpcd_usb.h
+++ xarpcd_usb.h
@@ -5,7 +5,7 @@
 #include "psock_proxy_msg.h"
 
 /* Define these values to match your devices */
-#define USB_VENDOR_ID_XAPTUM	 0x02fe
+#define USB_VENDOR_ID_XAPTUM	 0x2fe0
 #define USB_SUBCLASS_XAPTUM_PSOCK   0xab
 
 int xarpcd_send_msg( struct psock_proxy_msg *msg );
