4e7aa6e60cee75b33a59baf1eb89426b88b809ce
Daniel Berliner <daniel.berliner@xaptum.com>
2019-10-30 12:25:50 -0500

    Rename USB_SUBCLASS_SCM to USB_SUBCLASS_SCM_XAPTUM

--- xaprc00x-usb.h
+++ xaprc00x-usb.h
@@ -7,8 +7,7 @@
 #ifndef __XARPCD_USB_H_
 #define __XARPCD_USB_H_
 
-/* Define these values to match your devices */
 #define USB_VENDOR_ID_XAPTUM	 0x2fe0
-#define USB_SUBCLASS_SCM   0xab
+#define USB_SUBCLASS_SCM_XAPTUM   0xab
 
 #endif
